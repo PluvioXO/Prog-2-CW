979b3d23e8eaad02b87f4572b778a81123cc3bb5
Maximilian Nicholson <maxnicholson011@gmail.com>
2025-03-25 22:26:15 +0000

    security logic flaw

--- Classes/DBQuery.py
+++ Classes/DBQuery.py
@@ -3,29 +3,38 @@ class DBQuery:
         self.pss = pss
         self.eml = eml
 
-    def set_water(self, cups:int) -> bool:
+    def set_water(self, cups : int) -> bool:
         pass
 
-    def set_steps(self, steps:int) -> bool:
+    def set_steps(self, steps : int) -> bool:
         pass
 
-    def set_mood(self, mood:int) -> bool:
+    def set_mood(self, mood : int) -> bool:
         pass
 
-    def set_sleep(self, sleep:float) -> bool:
+    def set_sleep(self, sleep : float) -> bool:
         pass
 
-    def set_meaningful_work_hours(self, meaningful_work_hours:float) -> bool:
+    def set_meaningful_work_hours(self, meaningful_work_hours : float) -> bool:
         pass
 
     def get_water(self) -> int:
-        pass
+        try:
+            pass
+        except:
+            return 0
 
     def get_steps(self) -> int:
-        pass
+        try:
+            pass
+        except:
+            return 0
 
     def get_mood(self) -> int:
-        pass
+        try:
+            pass
+        except:
+            return 0
 
     def get_sleep(self) -> float:
         pass
