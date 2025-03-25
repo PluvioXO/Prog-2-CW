c186954f6b3a5d849dbddd7509efceca99ea96dc
OOPOnTop <charlieharrison2911@gmail.com>
2025-03-25 14:31:43 +0000

    Brief dashboard implementation, before 1 st sprint

---
+++ Classes/Goal.py
@@ -0,0 +1,24 @@
+class Goal:
+    def __init__(self):
+        self.name = ''
+        self.description = ''
+        self.progress = 0
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_description(self) -> str:
+        return self.description
+
+    def get_progress(self) -> int:
+        return self.progress
+
+    def set_name(self, name) -> None:
+        self.name = name
+
+    def set_description(self, description) -> None:
+        self.description = description
+
+    def update_progress(self, progress) -> None:
+        pass
+
