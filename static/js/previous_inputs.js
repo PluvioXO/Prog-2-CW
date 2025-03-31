a326f9ba1e67f25e03bc749170a0fed2b65657fd
OOPOnTop <charlieharrison2911@gmail.com>
2025-03-31 15:26:56 +0100

    Java functions for showing edit form and not

--- static/js/previous_inputs.js
+++ static/js/previous_inputs.js
@@ -38,4 +38,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+function editEntry(created_at, sleep, mood, screenTime, water, steps, work) {
+    console.log("Editing entry:", created_at);
+
+    document.getElementById("edit-form-container").classList.remove("hidden");
+    document.querySelector(".entries-container").classList.add("hidden");
+
+    document.getElementById("edit-entry-id").value = entryID;
+    document.getElementById("edit-sleep").value = sleep;
+    document.getElementById("edit-mood").value = mood;
+    document.getElementById("edit-screen-time").value = screenTime;
+    document.getElementById("edit-water").value = water;
+    document.getElementById("edit-steps").value = steps;
+    document.getElementById("edit-work").value = work;
+}
+
+function cancelEdit() {
+    document.getElementById("edit-form-container").classList.add("hidden");
+    document.querySelector(".entries-container").classList.remove("hidden");
+}
+
+function confirmDelete(entryID) {
+    if (confirm("Are you sure you want to delete this entry?")) {
+        //code to delete/redirect
+    }
+}
+
 
