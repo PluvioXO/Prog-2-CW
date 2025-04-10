3b1ce49fc728dd84e8023537f3cef91e613b6bf2
montana-rusa <emlcliffe@gmail.com>
2025-04-10 14:48:05 +0100

    goal input validation

--- static/js/goals.js
+++ static/js/goals.js
@@ -24,73 +24,86 @@ function changeGoal(metric) {
 
     if (metric == 1)  {
         if (isNumber(document.getElementById('goalChangeInput1').value)) {
-          sleepGoal = document.getElementById('goalChangeInput1').value;
-          document.getElementById('sleepText').innerHTML=("Current goal: " + sleepGoal + "    Current progress: " + sleepProgress);}
-
+            if ((document.getElementById('goalChangeInput1').value >= 0) && (document.getElementById('goalChangeInput1').value <= 24)) {
+                sleepGoal = document.getElementById('goalChangeInput1').value;
+                document.getElementById('sleepText').innerHTML=("Current goal: " + sleepGoal + "  |  Current progress: " + sleepProgress);
+            } else {
+                alert("Input must be between 0 and 24!") }
         progressBar = document.getElementById('P1');
             if (sleepProgress > sleepGoal) {
                 progressBar.style.width = "100%";
             } else {
                 progressBar.style.width = sleepGoal === 0 ? "0%" : ((sleepProgress / sleepGoal) * 100) + "%";
-            }
-    }
+            } } }
 
     if (metric == 2)  {
         if (isNumber(document.getElementById('goalChangeInput2').value)) {
-          moodGoal = document.getElementById('goalChangeInput2').value;
-          document.getElementById('moodText').innerHTML=("Current goal: " + moodGoal + "    Current progress: " + moodProgress);}
+            if  ((document.getElementById('goalChangeInput2').value >= 1) && (document.getElementById('goalChangeInput2').value <= 5)) {
+                moodGoal = document.getElementById('goalChangeInput2').value;
+                document.getElementById('moodText').innerHTML=("Current goal: " + moodGoal + "  |  Current progress: " + moodProgress);
+            } else {
+                alert("Mood must be between 0 and 5!") } 
 
         progressBar = document.getElementById('P2');
             if (moodProgress > moodGoal) {
                 progressBar.style.width = "100%";
             } else {
                 progressBar.style.width = moodGoal === 0 ? "0%" : ((moodProgress / moodGoal) * 100) + "%";
-            }
-    }
+            } } }
 
     if (metric == 3)  {
         if (isNumber(document.getElementById('goalChangeInput3').value)) {
-          screenGoal = document.getElementById('goalChangeInput3').value;
-          document.getElementById('screenText').innerHTML=("Current goal: " + screenGoal + "    Current progress: " + screenProgress);}
+            if ((isNumber(document.getElementById('goalChangeInput3').value >= 0)) && (isNumber(document.getElementById('goalChangeInput3').value <= 24))) {
+               screenGoal = document.getElementById('goalChangeInput3').value;
+               document.getElementById('screenText').innerHTML=("Current goal: " + screenGoal + "  |  Current progress: " + screenProgress);
+            } else {
+                alert("Screen time must be between 0 and 24!") }
 
         progressBar = document.getElementById('P3');
             if (screenProgress > screenGoal) {
                 progressBar.style.width = "100%";
             } else {
                 progressBar.style.width = screenGoal === 0 ? "0%" : ((screenProgress / screenGoal) * 100) + "%";
-            }
-    }
+            } } }
 
     if (metric == 4)  {
         if (isNumber(document.getElementById('goalChangeInput4').value)) {
-          waterGoal = document.getElementById('goalChangeInput4').value;
-          document.getElementById('waterText').innerHTML=("Current goal: " + waterGoal + "    Current progress: " + waterProgress);}
+            if ((isNumber(document.getElementById('goalChangeInput4').value >= 0)) && (isNumber(document.getElementById('goalChangeInput4').value <= 20))) {
+               waterGoal = document.getElementById('goalChangeInput4').value;
+               document.getElementById('waterText').innerHTML=("Current goal: " + waterGoal + "  |  Current progress: " + waterProgress);
+            } else {
+                alert("Water intake must be between 0 and 20!") }
 
         progressBar = document.getElementById('P4');
             if (waterProgress > waterGoal) {
                 progressBar.style.width = "100%";
             } else {
                 progressBar.style.width = waterGoal === 0 ? "0%" : ((waterProgress / waterGoal) * 100) + "%";
-            }
-    }
+            } } }
 
     if (metric == 5)  {
         if (isNumber(document.getElementById('goalChangeInput5').value)) {
-          stepGoal = document.getElementById('goalChangeInput5').value;
-          document.getElementById('stepText').innerHTML=("Current goal: " + stepGoal + "    Current progress: " + stepProgress);}
+            if ((isNumber(document.getElementById('goalChangeInput5').value >= 0)) && (isNumber(document.getElementById('goalChangeInput5').value <= 300000))) {
+               stepGoal = document.getElementById('goalChangeInput5').value;
+               document.getElementById('stepText').innerHTML=("Current goal: " + stepGoal + "  |  Current progress: " + stepProgress);
+            } else {
+                alert("Step count must be between 0 and 300,000!") }
 
         progressBar = document.getElementById('P5');
             if (stepProgress > stepGoal) {
                 progressBar.style.width = "100%";
             } else {
                 progressBar.style.width = stepGoal === 0 ? "0%" : ((stepProgress / stepGoal) * 100) + "%";
-            }
-    }
+            } } }
 
     if (metric == 6) {
         if (isNumber(document.getElementById('goalChangeInput6').value)) {
-          workGoal = document.getElementById('goalChangeInput6').value;
-          document.getElementById('workText').innerHTML=("Current goal: " + workGoal + "    Current progress: " + workProgress);}
+            if ((isNumber(document.getElementById('goalChangeInput6').value >= 0)) && (isNumber(document.getElementById('goalChangeInput6').value <= 24))) {
+               workGoal = document.getElementById('goalChangeInput6').value;
+               document.getElementById('workText').innerHTML=("Current goal: " + workGoal + "  |  Current progress: " + workProgress);
+            } else {
+                alert("Hours of meaningful work must be between 0 and 24!")
+            }
 
         progressBar = document.getElementById('P6');
             if (workProgress > workGoal) {
@@ -98,8 +111,8 @@ function changeGoal(metric) {
             } else {
                 progressBar.style.width = workGoal === 0 ? "0%" : ((workProgress / workGoal) * 100) + "%";
             } } }
-// visual changes when a new goal is step, currently implemented using the placeholder data
-
+}
+// visual changes when a new goal is set, currently implemented using the placeholder data
 
 function startUp() {
     changeGoal(1); changeGoal(2); changeGoal(3); changeGoal(4); changeGoal(5); changeGoal(6);
